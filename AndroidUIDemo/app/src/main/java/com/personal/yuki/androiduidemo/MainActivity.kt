4b99cd260b342f42caf6606e0e4431787a3e40ce
OkuderaYuki <>
2018-10-07 23:56:57 +0900

    [Add] TextViewのサンプルを実装しました

--- AndroidUIDemo/app/src/main/java/com/personal/yuki/androiduidemo/MainActivity.kt
+++ AndroidUIDemo/app/src/main/java/com/personal/yuki/androiduidemo/MainActivity.kt
@@ -2,11 +2,16 @@ package com.personal.yuki.androiduidemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // xmlで設定したIDを指定して、TextViewのインスタンスを取得する
+        val mTextView: TextView = findViewById(R.id.textView)
+        println(mTextView.text)
     }
 }
