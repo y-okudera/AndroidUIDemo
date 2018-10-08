c0a9133a8fee00391fc35b58f4222a863c1abed0
OkuderaYuki <>
2018-10-08 11:39:40 +0900

    [Add] Buttonのサンプルを実装しました

--- AndroidUIDemo/app/src/main/java/com/personal/yuki/androiduidemo/MainActivity.kt
+++ AndroidUIDemo/app/src/main/java/com/personal/yuki/androiduidemo/MainActivity.kt
@@ -2,7 +2,9 @@ package com.personal.yuki.androiduidemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +15,13 @@ class MainActivity : AppCompatActivity() {
         // xmlで設定したIDを指定して、TextViewのインスタンスを取得する
         val mTextView: TextView = findViewById(R.id.textView)
         println(mTextView.text)
+
+        val button: Button = findViewById(R.id.button)
+
+        // ボタンのタップイベントを登録する
+        button.setOnClickListener {
+            // トーストを表示する
+            Toast.makeText(this, "ボタンタップ", Toast.LENGTH_SHORT).show()
+        }
     }
 }
